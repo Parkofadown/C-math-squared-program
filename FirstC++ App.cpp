7e9df7656a9b5b082d77d1e7466f59f9f85fc0f0
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-04-03 20:10:23 -0400

    Add files via upload

---
+++ FirstC++ App.cpp
@@ -0,0 +1,39 @@
+// FirstC++ App.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+double square(double x)
+{
+	return x * x;
+}
+
+void print_square(double x)
+{
+	cout << "The square of " << x << " is " << square(x) << "\n";
+}
+
+int main()
+{
+	double x;
+
+	cout << "Enter a number, the result will be that number squared: ";
+	cin >> x;
+	square(x);
+	print_square(x);
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
